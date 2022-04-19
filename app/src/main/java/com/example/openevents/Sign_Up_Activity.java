073e1ebf73761ed68d1b217ac96eacabe89e2ea9
superfer2002 <fporcel2002@gmail.com>
2022-04-19 18:43:39 +0200

    Sign Up layout creation

---
+++ app/src/main/java/com/example/openevents/Sign_Up_Activity.java
@@ -0,0 +1,14 @@
+package com.example.openevents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_Up_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
