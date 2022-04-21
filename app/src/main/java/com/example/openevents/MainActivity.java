90e2e4b2fa4b372514643377ef64d7f4938b0842
superfer2002 <fporcel2002@gmail.com>
2022-04-21 17:48:55 +0200

    main activity_main.xml little changes

--- app/src/main/java/com/example/openevents/MainActivity.java
+++ app/src/main/java/com/example/openevents/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(intent);
                 MainActivity.this.finish();
             }
-        }, 3000);
+        }, 1800);
     }
 
 }
