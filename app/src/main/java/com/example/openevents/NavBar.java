98f3f21b02150d7adef1b3b5d44b9691c48364bd
emma <etarazg@gmail.com>
2022-05-06 12:22:12 +0200

    - Navigation bar implementation with fragments - icons on navbar with OnClickListener

--- app/src/main/java/com/example/openevents/NavBar.java
+++ app/src/main/java/com/example/openevents/NavBar.java
@@ -1,13 +1,61 @@
 package com.example.openevents;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class NavBar extends AppCompatActivity {
+import com.example.openevents.fragments.ExploreEventsFragment;
+import com.example.openevents.fragments.MyEventsFragment;
+import com.example.openevents.fragments.PeopleFragment;
+import com.example.openevents.fragments.PersonFragment;
+import com.example.openevents.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class NavBar extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    BottomNavigationView bottomNavView;
+
+    ExploreEventsFragment exploreEventsFragment = new ExploreEventsFragment();
+    MyEventsFragment myEventsFragment = new MyEventsFragment();
+    SearchFragment searchFragment = new SearchFragment();
+    PeopleFragment peopleFragment = new PeopleFragment();
+    PersonFragment personFragment = new PersonFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_bar);
+
+        bottomNavView = findViewById(R.id.container);
+
+        bottomNavView.setOnItemSelectedListener(this);
+        bottomNavView.setSelectedItemId(R.id.home);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.home) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, exploreEventsFragment).commit();
+            return true;
+        } else if (id == R.id.myevents) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, myEventsFragment).commit();
+            return true;
+        } else if (id == R.id.search) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, searchFragment).commit();
+            return true;
+        } else if (id == R.id.people) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, peopleFragment).commit();
+            return true;
+        } else if (id == R.id.person) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, personFragment).commit();
+            return true;
+        }
+
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/openevents/fragments/SearchFragment.java
@@ -0,0 +1,26 @@
+package com.example.openevents.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.openevents.R;
+
+
+public class SearchFragment extends Fragment {
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
